6fe9eab9bc65bac619e1c2a38afaf72044f62981
Aldair Alexis <aalexisdev11@gmail.com>
2025-08-12 01:35:44 -0500

    rdy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
